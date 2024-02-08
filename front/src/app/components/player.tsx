4813a232f7e6ba935c492d4611a81db0859657d3
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2024-02-07 23:20:00 -0300

    feat: Init camera

--- front/src/app/components/player.tsx
+++ front/src/app/components/player.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import { useRef } from "react";
+
 export default function Player() {
+  const userVideo = useRef<HTMLVideoElement>(null);
+
+  async function initCamera() {
+    const video = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true
+      }
+    });
+
+    if (userVideo.current) userVideo.current.srcObject = video;
+  }
+
+  initCamera();
+
   return (
     <div className="bg-gray-900 w-full h-full rounded-md p-2 relative">
-      <video className="h-full w-full"></video>
+      <video
+        className="h-full w-full"
+        autoPlay
+        playsInline
+        ref={userVideo}
+      />
       <span className="absolute bottom-3">{localStorage.getItem('username')}</span>
     </div>
   )
